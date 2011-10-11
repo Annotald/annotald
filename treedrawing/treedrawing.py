a4e3fd817f2a276b9b9870a0b758fb3bd43df7d3
Jana Beck <janabeck@babel.ling.upenn.edu>
2011-10-11 00:08:39 -0400

    changed README and added copyright info in treedrawing.py

--- treedrawing/treedrawing.py
+++ treedrawing/treedrawing.py
@@ -1,4 +1,15 @@
 # This Python file uses the following encoding: utf-8
+
+"""
+treedrawing.py
+Created 2011/10/10
+@author: Anton Karl Ingason
+@author: Jana E. Beck
+@copyright: GNU Lesser General Public License http://www.gnu.org/licenses/
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+@contact: jana.eliz.beck@gmail.com
+"""
+
 import os.path
 # current_dir = os.path.dirname(os.path.abspath(__file__))
 import re
