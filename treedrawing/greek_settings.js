d53740a9fb972b9f3389bb11af7e4e545c16657e
Jana Beck <janabeck@babel.ling.upenn.edu>
2011-11-27 01:31:07 -0600

    still working towards getting -PASS and other toggle dash tags at the POS level

--- treedrawing/greek_settings.js
+++ treedrawing/greek_settings.js
@@ -28,7 +28,7 @@ var caseTags=["N","NS","NPR","NPRS","PRO","D","NUM","ADJ","ADJR","ADJS","Q","QR"
 /* extensions are treated as not part of the label for various purposes, 
  * they are all binary, and they show up in the toggle extension menu  
  */
-var extensions=["-SPE","-PRN","-SBJ","-LFD","-RSP","-XXX","-ZZZ"];
+var extensions=["-SPE","-PRN","-SBJ","-LFD","-RSP","-XXX","-ZZZ","-PASS"];
 
 /*
  * Phrase labels in this list (including the same ones with indices and
