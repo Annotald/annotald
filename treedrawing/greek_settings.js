4846bf8d060b4f85b02328a8240d9e1f9f94cdff
Jana Beck <janabeck@babel.ling.upenn.edu>
2011-11-28 01:18:02 -0600

    major reworking of keyboard shortcuts in accordance with PDF guide

--- treedrawing/greek_settings.js
+++ treedrawing/greek_settings.js
@@ -51,24 +51,48 @@ var ipnodes=["IP-SUB","IP-MAT","IP-IMP","IP-INF","IP-PPL","RRC"];
 function customCommands(){
     // left hand commands
     addCommand({ keycode: 65 }, toggleVerbalExtension, "-AOR"); // a
-    addCommand({ keycode: 66 }, setLabel, ["ADVP", "ADVP-TMP", "ADVP-LOC", "ADVP-DIR"]); // b
+    // adverb phrase shortcuts
+    addCommand({ keycode: 66 }, setLabel, ["ADVP", "ADVP-DIR", "ADVP-LOC", "ADVP-TMP"]); // b
+    // adverbial CPs
+    addCommand({ keycode: 66, shift: true }, setLabel, ["CP-ADV","CP-PRP","CP-RES"]); // shift + b
     addCommand({ keycode: 67 }, coIndex); // c
     addCommand({ keycode: 68 }, pruneNode); // d
-    addCommand({ keycode: 69 }, setLabel, ["NP-PRN","NP-PAR","NP","NP-COM","NP-ATR"]); // e
+    addCommand({ keycode: 68, shift: true}, setLabel, ["CLPRT","INTJ","INTJP","PRTQ","FW","AN","KE"]); // shift + d
+    addCommand({ keycode: 68, ctrl: true}, setLabel, ["NEG"]); // ctrl + d
+    // NP-within-NP shortcuts
+    addCommand({ keycode: 69 }, setLabel, ["NP-ATR","NP-PRN","NP-PAR","NP-CMP","NP-COM"]); // e
     addCommand({ keycode: 69, shift: true }, setLabel, ["NP"]); // shift + e
+    addCommand({ keycode: 69, ctrl: true }, setLabel, ["NY"]); // ctrl + e
     addCommand({ keycode: 70 }, setLabel, ["PP"]); // f
     addCommand({ keycode: 70, shift: true }, toggleVerbalExtension, "-FUT"); // shift + f
-    addCommand({ keycode: 71 }, setLabel, ["ADJP","ADJP-PRD","ADJP-SPR"]); // g
+    addCommand({ keycode: 70, ctrl: true }, setLabel, "FRAG"); // ctrl + f
+    // adjective phrase shortcuts
+    addCommand({ keycode: 71 }, setLabel, ["ADJP","ADJP-PRD","ADJP-SPR","ADJX","ADJY"]); // g
     addCommand({ keycode: 71, shift: true }, setLabel, ["NP-AGT"]); // shift + g
 
     addCommand({ keycode: 81 }, setLabel, ["CONJP"]); // q
-    addCommand({ keycode: 82 }, setLabel, ["CP-REL","CP-FRL","CP-CAR","CP-EOP"]); // r
-    addCommand({ keycode: 83 }, setLabel, ["IP-SUB","IP-MAT","IP-IMP"]); // s
+    addCommand({ keycode: 81, shift: true}, setLabel, ["CP-QUE"]); // shift + q
+    addCommand({ keycode: 81, ctrl: true}, setLabel, ["QP","QTP","QX","QY"]); // ctrl + q
+    
+    // relative clauses and variations thereof
+    addCommand({ keycode: 82 }, setLabel, ["CP-REL","CP-CAR","CP-CMP","CP-EOP","CP-EXL","CP-FRL"]); // r
+    addCommand({ keycode: 82, shift: true }, setLabel, ["RRC"]); // shift + r
+    // basic sentence-level elements
+    addCommand({ keycode: 83 }, setLabel, ["IP-MAT","IP-IMP","IP-SUB"]); // s
     addCommand({ keycode: 83, shift: true}, setLabel, ["IP"]); // shift + s
-    addCommand({ keycode: 84 }, setLabel, ["CP-THT","CP-ADV","CP-CMP","CP-DEG"]); // t
+    addCommand({ keycode: 83, ctrl: true}, setLabel, ["IY"]); // ctrl + s
+    // -SPE IP-level extensions
+    addCommand({ keycode: 83, ctrl: true}, setLabel, ["IP-MAT-SPE","IP-IMP-SPE"]);
+    // complement CPs
+    addCommand({ keycode: 84 }, setLabel, ["CP-THT","CP-COM","CP-DEG"]); // t
 
-    addCommand({ keycode: 86 }, setLabel, ["IP-INF","IP-INF-COM","IP-SMC","IP-PPL","IP-INF-THT","IP-INF-PRP"]); // v
-    addCommand({ keycode: 87 }, setLabel, ["NP-SBJ","NP-OB1","NP-OB2","NP-PRD","NP-OBP","NP-OBQ","NP"]); // w
+    // participial clauses
+    addCommand({ keycode: 86 }, setLabel, ["IP-PPL","IP-ABS","IP-SMC","IP-PPL-COM"]); // v
+    // infinitive clauses
+    addCommand({ keycode: 86, shift: true }, setLabel, ["IP-INF","IP-INF-COM","IP-INF-PRP","IP-INF-SBJ","IP-INF-ABS"]); // shift + v
+    // argument NP shortcuts
+    addCommand({ keycode: 87 }, setLabel, ["NP-SBJ","NP-OB1","NP-OB2","NP-OBP","NP-OBQ","NP-PRD"]); // w
+    addCommand({ keycode: 87, shift: true }, setLabel, ["WADJP","WADVP","WNP","WPP","WQP"]); // shift + w
     addCommand({ keycode: 88 }, makeNode, "XP"); // x
     addCommand({ keycode: 88, shift: true }, setLabel, ["XP"]); // shift + x
 
@@ -77,7 +101,8 @@ function customCommands(){
     // left hand number commands
     addCommand({ keycode: 49 }, leafBefore); // 1
     addCommand({ keycode: 50 }, leafAfter); // 2
-    addCommand({ keycode: 51 }, setLabel, ["NP-INS","NP-TMP","NP-LOC","NP-ADV","NP-MSR","NP-AGT","NP-CMP","NP-DIR","NP-ADT","NP-VOC","NP"]); // 3
+    // non-argument NP shortcuts
+    addCommand({ keycode: 51 }, setLabel, ["NX","NP-ADT","NP-ADV","NP-AGT","NP-DIR","NP-INS","NP-LOC","NP-MSR","NP-SPR","NP-TMP","NP-VOC"]); // 3
     addCommand({ keycode: 52 }, toggleExtension, "-PRN"); // 4
     addCommand({ keycode: 53 }, toggleExtension, "-SPE"); // 5
 
@@ -102,47 +127,46 @@ function customCommands(){
     addCommand({ keycode: 32 }, clearSelection); // spacebar
     addCommand({ keycode: 192 }, toggleLemmata); // `
 
-    // An example of a context-sensitive label switching command.  If
-    // neither NP or PP is the POS, the NP value (first in the dictionary)
-    // is chosen by default.
-    // addCommand({ keycode: 123 } , setLabel, { NP: ["NP-SBJ", "NP-OB1", "NP-OB2"],
-    //                                           PP: ["PP-SBJ", "PP-OB1", "PP-OB2"]});
-
-// addCommand(51,"makenode","NP","NP-PRD","NP-POS"); // 3
-// addCommand(188,"clearselection"); // <
-// addCommand(78, "makenode","XP"); // n
-// addCommand(49,"redo"); // 1
 }
 
 
 /*
  * Default phrase label suggestions in context menu 
  */
-var defaultConMenuGroup = ["VBP","VBPP","VBD","VBDP","VBN","VBNP","VBS","VBSP","VBO","VBOP","VBI","VBIP"];
+var defaultConMenuGroup = ["VBP","VBPP","VBD","VBDP","VBN","VBNP","VBS","VBSP","VBO","VBOP","VBI","VBIP","VBDX","VBIX","VBNX","VBOX","VBPX","VBSX"];
 
 /**
  * Phrase labels that are suggested in context menu when one of the other ones is set
  */
 function customConMenuGroups(){
-	addConMenuGroup( ["IP-SUB","IP-MAT","IP-INF","IP-IMP","CP-QUE","QTP","FRAG"] );
-// add context menu group for IP-PPLs and IP-INFs
-	addConMenuGroup( ["ADJP","ADJX","NP-MSR","QP","NP","ADVP","IP-PPL"] );
-        addConMenuGroup( ["NP","NX","NP-SBJ","NP-OB1","NP-OB2","NP-OBP","NP-OBQ","NP-PRD","NP-ATR","NP-PAR","NP-COM","NP-PRN", "NP-SPR"] );
-        addConMenuGroup( ["NP-INS","NP-TMP","NP-LOC","NP-ADV","NP-MSR","NP-AGT","NP-CMP","NP-DIR","NP-ADT","NP-VOC","QP"] );
-	addConMenuGroup( ["PP","ADVP","ADVP-TMP","ADVP-LOC","ADVP-DIR","NP-MSR","NP-ADV"] );	
-	addConMenuGroup( ["P","ADV","ADVR","ADVS","ADJ","ADJR","ADJS","C","CONJ"] );
-	addConMenuGroup( ["WADVP","WNP","WPP","WQP","WADJP"] );
-        addConMenuGroup( ["CP-THT","CP-QUE","CP-REL","CP-DEG","CP-ADV","CP-CMP","CP-COM"] );
-        addConMenuGroup( ["N","N$","NA","ND","NS","NS$","NSA","NSD"] );
-        addConMenuGroup( ["ADJ","ADJ$","ADJA","ADJD","Q","Q$","QA","QD"] );
-        addConMenuGroup( ["PRO","PRO$","PROA","PROD","CLPRO$","CLPROA","CLPROD"] );
-        addConMenuGroup( ["VPR","VPRP","VPR$","VPRP$","VPRA","VPRPA","VPRD","VPRPD"] );
+    addConMenuGroup( ["ADJ","ADJ$","ADJA","ADJD","ADJR","ADJS"] );
+    addConMenuGroup( ["ADJP","ADJP-PRD","ADJP-SPR","ADJX","ADJY"] );
+    addConMenuGroup( ["ADV","ADVR","ADVS","NEG","P"] );
+    addConMenuGroup( ["ADVP","ADVP-DIR","ADVP-LOC","ADVP-TMP","PP"] );
+    addConMenuGroup( ["AN","KE"] );
+    addConMenuGroup( ["BED","BEI","BEN","BEO","BEP","BES","BPR"] );
+    addConMenuGroup( ["CLGE","CLPRO","CLPRO$","CLPROA","CLPROD","CLPRT","CLQ","CLQ$","CLQA","CLQD","CLTE"] );
+    addConMenuGroup( ["C","CONJ","CONJ0"] );
+    addConMenuGroup( ["D","D$","DA","DD","DS","DS$","DSA","DSD"] );
+    addConMenuGroup( ["INTJ","INTJP","FW","PRTQ"] );
+    addConMenuGroup( ["Q","Q$","QA","QD","QR","QS","QV"] );
+    addConMenuGroup( ["NUM","N","N$","NA","ND","NPR","NPR$","NPRA","NPRD","NPRS","NPRS$","NPRSA","NPRSD","NS","NS$","NSA","NSD"] );
+    addConMenuGroup( ["NX","NY","NP-ADT","NP-ADV","NP-AGT","NP-DIR","NP-INS","NP-LFD","NP-LOC","NP-MSR","NP-RSP","NP-SPR","NP-TMP","NP-VOC","QP"] );
+    addConMenuGroup( ["NP-SBJ","NP-OB1","NP-OB2","NP-OBP","NP-OBQ","NP-PRD","NP-ATR","NP-PRN","NP-PAR","NP-CMP","NP-COM"] );
+    addConMenuGroup( ["VPR","VPRP","VPR$","VPRP$","VPRA","VPRPA","VPRD","VPRPD"] );
+    addConMenuGroup( ["WADJ","WADJ$","WADJA","WADJD","WADV","WD","WD$","WDA","WDD","WP","WPRO","WPRO$","WPROA","WPROD","WQ"] );
+    addConMenuGroup( ["WADJP","WADJX","WADVP","WADVX","WNP","WNX","WPP","WQP"] );
+    addConMenuGroup( ["CP","CP-ADV","CP-CAR","CP-COM","CP-CMP","CP-DEG","CP-EOP","CP-EXL","CP-FRL","CP-PRP","CP-QUE","CP-REL","CP-RES","CP-THT"] );
+    addConMenuGroup( ["IP","IY","RRC","IP-ABS","IP-IMP","IP-INF","IP-INF-COM","IP-INF-PRP","IP-INF-SBJ","IP-MAT","IP-PPL","IP-PPL-COM","IP-SMC","IP-SUB"] );
+    addConMenuGroup( ["QP","QX","QY"] );
+    //addConMenuGroup( [] );
 }
 
 /*
  * Context menu items for "leaf before" shortcuts
  */
 function customConLeafBefore(){
+        addConLeafBefore( "CODE", "{BKMK}");
 	addConLeafBefore( "NP-SBJ", "*con*");
 	addConLeafBefore( "NP-SBJ", "*pro*");
         addConLeafBefore( "NP-SBJ", "*");
@@ -152,7 +176,6 @@ function customConLeafBefore(){
 	addConLeafBefore( "WNP", "0");
 	addConLeafBefore( "WADJP", "0");
 	addConLeafBefore( "C", "0");
-        addConLeafBefore( "CODE", "{BKMK}");
 	addConLeafBefore( "CODE", "{COM:");	
 	addConLeafBefore( "CODE", "{TODO:");
 	addConLeafBefore( "CODE", "{MAN:");	
