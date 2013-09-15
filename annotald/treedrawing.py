f495871a5e82149c5b518f47871e7a8af916d178
Aaron Ecay <aaronecay@gmail.com>
2013-09-15 14:08:53 -0400

    treedrawing.py: change contact email to Aaron Ecay

--- annotald/treedrawing.py
+++ annotald/treedrawing.py
@@ -11,7 +11,7 @@ version.  http://www.gnu.org/licenses/ This program is distributed in the hope
 that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
-@contact: jana.eliz.beck@gmail.com
+@contact: aaronecay@gmail.com
 """
 
 from __future__ import unicode_literals
