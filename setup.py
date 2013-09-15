0c3eb0f7ac75604623370ea1ed0674fdd14fb0a7
Aaron Ecay <aaronecay@gmail.com>
2013-09-15 12:20:32 -0400

    setup.py: various fixes

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from distutils.core import setup
 # but the former has a very stupid quirk whereby package_data is not put
 # in the sdist.
 
-
-from distutils.core import setup
 import os
+import annotald
 
 setup_args = {
       'name': 'Annotald'
-    , 'version': '1.0.dev1'
+    , 'version': annotald.__version__
     , 'author': 'Aaron Ecay, Anton Karl Ingason and Jana Beck'
     , 'author_email': 'aaronecay@gmail.com'
     , 'url': 'http://annotald.github.com/'
@@ -20,7 +19,6 @@ setup_args = {
 
 if os.name == "nt":
     import py2exe
-    import shutil
     import glob
 
     manifest = """
@@ -51,27 +49,27 @@ manifestVersion="1.0">
 
     annotald_data_files = []
 
-    annotald_data_files.append( ('', ['settings.js','CS_Tony_oct19.jar',
-                                      'nothing.q','user-plain-red.png',
+    annotald_data_files.append( ('', ['settings.js', 'CS_Tony_oct19.jar',
+                                      'nothing.q', 'user-plain-red.png',
                                       'user-plain-blue.png']) )
 
-    html_files =  glob.glob('data/html/*.mako')
+    html_files = glob.glob('data/html/*.mako')
     annotald_data_files.append( ('data/html', html_files) )
 
-    image_files =  glob.glob('data/images/*.png')
+    image_files = glob.glob('data/images/*.png')
     annotald_data_files.append( ('data/images', image_files) )
 
-    script_files =  glob.glob('data/scripts/*.js')
+    script_files = glob.glob('data/scripts/*.js')
     annotald_data_files.append( ('data/scripts', script_files) )
 
-    css_files =  glob.glob('data/css/*.css')
+    css_files = glob.glob('data/css/*.css')
     annotald_data_files.append( ('data/css', css_files) )
 
     setup(windows=[{ "script": "annotald-win.py",
                      "other resources": [24,1, manifest]
-                 }],
+                   }],
           data_files = annotald_data_files,
-          options={ "py2exe":{ "skip_archive": True } },
+          options={ "py2exe": { "skip_archive": True } },
           **setup_args)
 
 else:
@@ -82,10 +80,7 @@ else:
         , package_data = { 'annotald': ["data/*/*", "settings.py",
                                         "settings.js",
                                         "CS_Tony_oct19.jar"] }
-        , install_requires = ["nltk",
-                              "numpy", # NLTK lyingly claims not to
-                                       # require numpy
-                              "mako", "cherrypy", "setuptools"]
+        , requires = ["mako", "cherrypy", "setuptools", "lovett", "argparse"]
         , provides = ["annotald"]
         , **setup_args
     )
